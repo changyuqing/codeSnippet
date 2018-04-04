fec18924f940baf30864397eecd285553715e356
changyuqing <changyuqing_@outlook.com>
2018-04-05 01:44:51 +0800

    实现Promise（三）then 的异步调用，穿透，链式传递

--- MyPromise.js
+++ MyPromise.js
@@ -1,44 +1,136 @@
-export function MyPromise (executor) {
+export function MyPromise(executor) {
 	const self = this;
-	self.state = "pending";// pending, fulfilled 或 rejected.
+	self.status = "pending"; // pending, fulfilled 或 rejected.
 	self.value = "";
 	self.reason = "";
-	self.fulfilledBus=[];
-	self.rejectedBus=[];
-	function reslove(data){
-		if(self.state==="pending"){
-			self.state="fulfilled";
+	self.fulfilledBus = [];
+	self.rejectedBus = [];
+
+	function resolve(data) {
+		if (self.status === "pending") {
+			self.status = "fulfilled";
 			self.value = data;
 			self.fulfilledBus.forEach(fn => {
-				fn(self.value);
+				fn();
 			});
 		}
 	}
-	function reject(data){
-		if(self.state==="pending"){
-			self.state="rejected";
+
+	function reject(data) {
+		if (self.status === "pending") {
+			self.status = "rejected";
 			self.reason = data;
 			self.rejectedBus.forEach(fn => {
-				fn(self.reason);
+				fn();
 			});
 		}
 	}
 	try {
-		executor(reslove,reject)
+		executor(resolve, reject)
 	} catch (error) {
 		reject(error)
 	}
 }
-MyPromise.prototype.then = function(reslove,reject){
-	if(this.state==="fulfilled"){
-		reslove(this.value);
-	}else if(this.state==="rejected"){
-		reject(this.reason);
+MyPromise.prototype.then = function (onFulfilled, onRejected) {
+	const self = this;
+	onFulfilled = typeof onFulfilled === 'function' ? onFulfilled : function (value) {
+		return value;
+	};
+	onRejected = typeof onRejected === 'function' ? onRejected : function (err) {
+		throw err;
+	};
+
+	let promise2;
+	if (self.status === "fulfilled") {
+		promise2 = new MyPromise(function (resolve, reject) {
+			setTimeout(function () {
+				try {
+					let x = onFulfilled(self.value);
+					resolvePromise(promise2, x, resolve, reject);
+				} catch (error) {
+					reject(error);
+				}
+			});
+
+		})
+
+	}
+	if (self.status === "rejected") {
+		promise2 = new MyPromise(function (resolve, reject) {
+			setTimeout(function () {
+				try {
+					let x = onRejected(self.reason);
+					resolvePromise(promise2, x, resolve, reject);
+				} catch (error) {
+					reject(error);
+				}
+			});
+
+		})
 	}
 
-	if(this.state==="pending"){
-		this.fulfilledBus.push((value)=>{reslove(value)});
-		this.rejectedBus.push((reason)=>{reject(reason)});
+	if (self.status === "pending") {
+		promise2 = new MyPromise(function (resolve, reject) {
+			self.fulfilledBus.push(function () {
+				setTimeout(function () {
+					try {
+						let x = onFulfilled(self.value)
+						resolvePromise(promise2, x, resolve, reject);
+					} catch (error) {
+						reject(error);
+					}
+				});
+
+			});
+			self.rejectedBus.push(function () {
+				setTimeout(function () {
+					try {
+						let x = onRejected(self.reason)
+						resolvePromise(promise2, x, resolve, reject)
+					} catch (error) {
+						reject(error);
+					}
+				});
+
+			});
+		})
 	}
+	return promise2;
 }
-		
+
+function resolvePromise(promise, x, resolve, reject) {
+	if(x===undefined){
+		resolve(x);
+	}
+	if (promise === x) {
+		return reject("TypeError")
+	}
+	let called = false;
+	if (x !== null && (typeof x === 'object' || typeof x === 'function')) {
+		try {
+			let then = x.then;
+			if (typeof then === 'function') {
+				then.call(x, function (y) {
+					if (called) return;
+					called = true;
+					resolvePromise(promise, y, resolve, reject);
+				}, function (r) {
+					if (called) return;
+					called = true;
+					reject(r)
+				});
+			} else {
+				resolve(x);
+			}
+		} catch (error) {
+			if (called) return;
+			called = true;
+			reject(error);
+		}
+
+
+	} else {
+		resolve(x);
+	}
+
+}
